4da04b361c87273ad4935bde7fdebbfbfb3bb235
Lashi <lashi0812@gmail.com>
2024-10-07 17:49:57 +0000

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main className="flex flex-col items-center px-3 pt-36">
+      <h1 className="text-3xl lg:text-6xl tracking-tight font-bold ">
+        Find Events around you
+      </h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Browse more than{" "}
+        <span className="text-bold italic underline text-[#a4f839]">
+          10,000 events
+        </span>{" "}
+        around you
+      </p>
+      <form className="w-full sm:w-[580px]">
+        <input
+          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50  transition focus:ring-2 focus:bg-white/20"
+          placeholder="Search events in your city..."
+          spellCheck={false}
+        ></input>
+      </form>
+      <section className="mt-4 flex gap-x-4 text-sm text-white/50">
+        <p>Popular:</p>
+        <div className="space-x-2 font-semibold">
+          <Link href="/events/austin">Austin</Link>
+          <Link href="/events/seattle">Seattle</Link>
+        </div>
+      </section>
+    </main>
+  );
 }
