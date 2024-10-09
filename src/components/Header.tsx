4caef1593cb82f8d69b834d8a9d0b955487e17cd
Lashi <lashi0812@gmail.com>
2024-10-09 06:07:55 +0000

    added the framer motion active bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
   { name: "Home", path: "/" },
@@ -7,17 +12,30 @@ const routes = [
 ];
 
 export default function Header() {
+  const activePathName = usePathname();
   return (
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
+      <nav className="h-full">
         <ul className="flex gap-x-6 h-full text-sm">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white items-center transition"
+              className={clsx(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white": activePathName === route.path,
+                  "text-white/50": activePathName !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+              {activePathName === route.path && (
+                <motion.div
+                  layoutId="active-link-bar"
+                  className="absolute bottom-0 left-0 right-0 h-0.5 bg-accent"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
